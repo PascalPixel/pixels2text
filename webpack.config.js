17902360d7297e6dc6e03edebe01244155bdae8f
Pascal Pencil <pascal@superpencil.com>
2016-12-25 16:45:57 +0900

    Trying to make a production Github Pages build with Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,78 @@
-module.exports = {
-  entry: ['./src/index.js'],
+const webpack = require('webpack')
+const merge = require('webpack-merge')
+const path = require('path')
+
+const PATHS = {
+  entry: path.resolve(__dirname, 'src'),
+  output: __dirname
+}
+
+const common = {
+  entry: PATHS.entry,
   output: {
-    path: __dirname,
-    publicPath: '/',
-    filename: 'bundle.js'
+    filename: 'bundle.js',
+    path: PATHS.output
   },
   module: {
-    loaders: [
+    rules: [
       {
-        exclude: /node_modules/,
-        loader: 'babel',
-        query: {
-          presets: ['react', 'es2015', 'stage-1']
+        loader: 'babel-loader',
+        options: {
+          presets: ['react', 'es2015', 'stage-0']
         }
       }
     ]
   },
   resolve: {
-    extensions: ['', '.js', '.jsx']
+    modules: [
+      'node_modules'
+    ],
+    extensions: ['.js', '.jsx', '.png']
   },
-  devServer: {
-    inline: true,
-    historyApiFallback: true,
-    contentBase: './'
+  target: 'web'
+}
+
+const development = () => {
+  return merge(common, {
+    output: {
+      publicPath: '/'
+    },
+    devServer: {
+      inline: true,
+      historyApiFallback: true,
+      contentBase: PATHS.dev
+    },
+    devtool: 'eval-source-map'
+  })
+}
+
+const production = () => {
+  return merge(common, {
+    output: {
+      publicPath: '/pixels2text/'
+    },
+    devtool: 'source-map',
+    plugins: [
+      new webpack.optimize.UglifyJsPlugin({
+        compress: {
+          screw_ie8: true,
+          warnings: false
+        }
+      }),
+      new webpack.DefinePlugin({
+        'process.env': {
+          'NODE_ENV': JSON.stringify('production')
+        }
+      })
+    ]
+  })
+}
+
+module.exports = (env) => {
+  if (env === 'production') {
+    console.log('Making production build...')
+    return production()
   }
+
+  return development()
 }
